8c76079e3b09638bd2a2a6437b1e4973708bd6df
UswaFatima89 <hoorf270@gmail.com>
2020-07-13 07:28:45 +0500

    assisgnment 1

---
+++ php1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta >
+	<title>first php class</title>
+</head>
+<body>
+	 <?php
+	 	$student1 = array(
+			 'name' =>  Uswa;
+			 'marks' => 90.05;
+		 );
+		 $student2 = array(
+			'name' =>  Bisma;
+			'subject' => ENGLISH;
+			'marks' => 95.75;
+		 );
+		 $merged_array = array_merge($student1,$student2);
+		 foreach($merged_array as $value){
+			 echo $value
+		 }
+	 ?>
+</body>
+</html>
